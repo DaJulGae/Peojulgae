9a4fad4d0390339a5ef52d60cef46b529b0ea33d
Minuffy <128578002+Minu0803@users.noreply.github.com>
2024-07-30 20:31:48 +0900

    홈화면 방문하기 버튼 지도 활성화

--- app/src/main/java/fragment/Frag1.java
+++ app/src/main/java/fragment/Frag1.java
@@ -1,15 +1,18 @@
 package fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.peojulgae.MapActivity;
 import com.example.peojulgae.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -32,10 +35,21 @@ public class Frag1 extends Fragment {
         // 로그인된 사용자 정보 가져와서 TextView 업데이트
         if (currentUser != null) {
             String userId = currentUser.getEmail();
-            welcomeTextView.setText(userId+ "님 환영합니다");
+            welcomeTextView.setText(userId + "님 환영합니다");
         } else {
             welcomeTextView.setText("로그인된 사용자가 없습니다.");
         }
+
+        // VisitButton 찾아서 클릭 이벤트 설정
+        Button visitButton = view.findViewById(R.id.VisitButton);
+        visitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // MapActivity로 전환
+                Intent intent = new Intent(getActivity(), MapActivity.class);
+                startActivity(intent);
+            }
+        });
 
         return view;
     }
