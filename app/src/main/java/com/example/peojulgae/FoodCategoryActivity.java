a570bec8742fef0dd3dcfba7b0e350ec7e865984
Minuffy <128578002+Minu0803@users.noreply.github.com>
2024-09-13 22:52:00 +0900

    다른 이미지 1차적으로 들어가는거까지 성공

--- app/src/main/java/com/example/peojulgae/FoodCategoryActivity.java
+++ app/src/main/java/com/example/peojulgae/FoodCategoryActivity.java
@@ -5,86 +5,107 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.tabs.TabLayout;
 
 public class FoodCategoryActivity extends AppCompatActivity {
+    ImageView ImageView1, ImageView2, ImageView3, ImageView4, ImageView5, ImageView6;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.food_category);
 
-        ImageView peachImageView = findViewById(R.id.peachimage);
-        ImageView appleImageView = findViewById(R.id.honeyapple);
-        ImageView watermelonImageView = findViewById(R.id.watermelonimage);
-        ImageView kiwiImageView = findViewById(R.id.kiwiimage);
-        ImageView shinemuscatImageView = findViewById(R.id.shinemuscatimage);
-        ImageView mangoImageView = findViewById(R.id.mangoimage);
-        // 더 많은 ImageView 변수가 필요한 경우 추가
+        ImageView1 = findViewById(R.id.peachimage);
+        ImageView2 = findViewById(R.id.honeyapple);
+        ImageView3 = findViewById(R.id.watermelonimage);
+        ImageView4 = findViewById(R.id.kiwiimage);
+        ImageView5 = findViewById(R.id.shinemuscatimage);
+        ImageView6 = findViewById(R.id.mangoimage);
 
-        peachImageView.setOnClickListener(new View.OnClickListener() {
+        TabLayout tabLayout = findViewById(R.id.food_tab_layout);
+
+        // Add Tab Selection Listener
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(FoodCategoryActivity.this, MartSpecific.class);
-                intent.putExtra("image_resource", R.drawable.peach);
-                intent.putExtra("item_name", "납작 복숭아");
-                intent.putExtra("item_price", "34,120원");
-                startActivity(intent);
+            public void onTabSelected(TabLayout.Tab tab) {
+                switch (tab.getText().toString()) {
+                    case "야채":
+                        updateVegetableImages();
+                        break;
+                    case "과일":
+                        updateFruitImages();
+                        break;
+                    case "육류":
+                        updateMeatImages(); // 육류 탭 선택 시 이미지 변경
+                        break;
+                    case "기타":
+                        updateElsesImages(); // 기타 탭 선택 시 이미지 변경
+                        break;
+                    // Add other cases for different categories
+                }
             }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) { }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) { }
         });
 
-        appleImageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(FoodCategoryActivity.this, MartSpecific.class);
-                intent.putExtra("image_resource", R.drawable.apple);
-                intent.putExtra("item_name", "꿀 사과");
-                intent.putExtra("item_price", "24,320원");
-                startActivity(intent);
-            }
-        });
+        setupImageViewClickListeners();
+    }
 
-        watermelonImageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(FoodCategoryActivity.this, MartSpecific.class);
-                intent.putExtra("image_resource", R.drawable.watermelon);
-                intent.putExtra("item_name", "수박");
-                intent.putExtra("item_price", "40,000원");
-                startActivity(intent);
-            }
-        });
-        kiwiImageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(FoodCategoryActivity.this, MartSpecific.class);
-                intent.putExtra("image_resource", R.drawable.kiwi);
-                intent.putExtra("item_name", "키위");
-                intent.putExtra("item_price", "25,000원");
-                startActivity(intent);
-            }
-        });
+    private void updateFruitImages() {
+        ImageView1.setImageResource(R.drawable.peach);
+        ImageView2.setImageResource(R.drawable.apple);
+        ImageView3.setImageResource(R.drawable.watermelon);
+        ImageView4.setImageResource(R.drawable.kiwi);
+        ImageView5.setImageResource(R.drawable.shine_muscat);
+        ImageView6.setImageResource(R.drawable.mango);
+    }
 
-        shinemuscatImageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(FoodCategoryActivity.this, MartSpecific.class);
-                intent.putExtra("image_resource", R.drawable.shine_muscat);
-                intent.putExtra("item_name", "샤인머스캣");
-                intent.putExtra("item_price", "40,000원");
-                startActivity(intent);
-            }
-        });
+    private void updateVegetableImages() {
+        ImageView1.setImageResource(R.drawable.carrot); // Example vegetable image
+        ImageView2.setImageResource(R.drawable.carrot);
+        ImageView3.setImageResource(R.drawable.carrot);
+        ImageView4.setImageResource(R.drawable.carrot);
+        ImageView5.setImageResource(R.drawable.carrot);
+        ImageView6.setImageResource(R.drawable.carrot);
+    }
 
-        mangoImageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(FoodCategoryActivity.this, MartSpecific.class);
-                intent.putExtra("image_resource", R.drawable.mango);
-                intent.putExtra("item_name", "망고");
-                intent.putExtra("item_price", "24,320원");
-                startActivity(intent);
-            }
-        });
+    private void updateMeatImages() {
+        ImageView1.setImageResource(R.drawable.meat_cow); // 고기 이미지로 변경
+        ImageView2.setImageResource(R.drawable.meat_cow);
+        ImageView3.setImageResource(R.drawable.meat_cow);
+        ImageView4.setImageResource(R.drawable.meat_cow);
+        ImageView5.setImageResource(R.drawable.meat_cow);
+        ImageView6.setImageResource(R.drawable.meat_cow);
+    }
 
-        // 더 많은 onClickListeners를 추가
+    private void updateElsesImages() {
+        ImageView1.setImageResource(R.drawable.mealkit); // 고기 이미지로 변경
+        ImageView2.setImageResource(R.drawable.mealkit);
+        ImageView3.setImageResource(R.drawable.mealkit);
+        ImageView4.setImageResource(R.drawable.mealkit);
+        ImageView5.setImageResource(R.drawable.mealkit);
+        ImageView6.setImageResource(R.drawable.mealkit);
+    }
+
+    private void setupImageViewClickListeners() {
+        ImageView1.setOnClickListener(v -> openMartSpecificActivity(R.drawable.peach, "납작 복숭아", "34,120원"));
+        ImageView2.setOnClickListener(v -> openMartSpecificActivity(R.drawable.apple, "꿀 사과", "24,320원"));
+        ImageView3.setOnClickListener(v -> openMartSpecificActivity(R.drawable.watermelon, "수박", "40,000원"));
+        ImageView4.setOnClickListener(v -> openMartSpecificActivity(R.drawable.kiwi, "키위", "25,000원"));
+        ImageView5.setOnClickListener(v -> openMartSpecificActivity(R.drawable.shine_muscat, "샤인머스캣", "40,000원"));
+        ImageView6.setOnClickListener(v -> openMartSpecificActivity(R.drawable.mango, "망고", "24,320원"));
+
+    }
+
+    private void openMartSpecificActivity(int imageResource, String itemName, String itemPrice) {
+        Intent intent = new Intent(FoodCategoryActivity.this, MartSpecific.class);
+        intent.putExtra("image_resource", imageResource);
+        intent.putExtra("item_name", itemName);
+        intent.putExtra("item_price", itemPrice);
+        startActivity(intent);
     }
 }
